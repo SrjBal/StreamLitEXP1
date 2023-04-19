e0c9f8622377ac702d3df58b9b655121ff430126
SrjBal <131257252+SrjBal@users.noreply.github.com>
2023-04-19 22:56:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,17 @@ This app calculates the largest of 3 given numbers
 n1 = st.number_input("First Number")
 n2 = st.number_input("Second Number")
 n3 = st.number_input("Third Number")
+
+if ( n1 >= n2 ):
+  if ( n1 >= n3 ):
+    ans = n1
+  else:
+    ans = n3
+else:
+  if ( n2 >= n3 ):
+    ans = n2
+  else:
+    ans = n3
+ 
+st.write(ans)
+    
