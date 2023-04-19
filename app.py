3bb13c20c195209db5d6a7b04b881f969af5711e
SrjBal <131257252+SrjBal@users.noreply.github.com>
2023-04-19 22:49:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ st.write("""
 
 This app calculates the largest of 3 given numbers
 """)
+n1 = st.number_input("First Number")
+n2 = st.number_input("Second Number")
+n3 = st.number_input("Third Number")
